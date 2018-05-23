6f2acfb7c3fb5039baec8a6aead7f4221b36c5ad
zombieboyh <38679770+zombieboyh@users.noreply.github.com>
2018-05-23 13:35:12 +0800

    4
    2016053020

---
+++ holle word/Assets/Scripts/移动.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class 移动 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		interesting conscynu  cnsefhjsk()，inshgkalsjf
+		sjfja.,asfag,asfdasf.gs.
+		wqrqrfklawkjjg.wqak;qakfga.awklfawkf.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
